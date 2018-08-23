11a851874e5eef0963777530f84e6c43c1be9a78
quelladai <40414229+quelladai@users.noreply.github.com>
2018-08-23 14:15:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class DataGenerator(ImageDataGenerator):
     def flow_from_directory(self, directory, *args, **kwargs):
         generator = super().flow_from_directory(directory, class_mode=None, *args, **kwargs)
         while True:
-            ori = next(generator)
+            temporal = next(generator)
 
             data_gen_args = dict(rotation_range=20,
                                  width_shift_range=0.2,
@@ -52,21 +52,13 @@ class DataGenerator(ImageDataGenerator):
                 batch_size=BATCH_SIZE,
                 target_size=(512, 512),
                 class_mode=None,
+                shuffle=True,
                 seed=1)
 
             masked = next(masked_generator)
 
-            # temporal_generator = datagen.flow_from_directory(
-            #     temporal_DIR,
-            #     batch_size=BATCH_SIZE,
-            #     target_size=(512, 512),
-            #     class_mode=None,
-            #     seed=1)
-            # temporal = next(temporal_generator)
-            temporal = deepcopy(ori)
-
             gc.collect()
-            yield [masked, temporal], ori
+            yield [masked, temporal]
 
 
 train_datagen = DataGenerator(
@@ -76,15 +68,32 @@ train_datagen = DataGenerator(
     rescale=1. / 255,
     horizontal_flip=True
 )
-train_generator = train_datagen.flow_from_directory(
-    label_DIR, target_size=(512, 512),batch_size=BATCH_SIZE,seed=1
-)
+train_generator_1 = train_datagen.flow_from_directory(temporal_DIR,
+                                                      target_size=(512, 512),
+                                                      batch_size=BATCH_SIZE,
+                                                      shuffle=True,
+                                                      seed=1
+                                                      )
+
+train_gen_args = dict(rotation_range=20,
+                     width_shift_range=0.2,
+                     height_shift_range=0.2,
+                     rescale=1. / 255,
+                     horizontal_flip=True)
+train_label_datagen = ImageDataGenerator(**train_gen_args)
+train_generator_2 = train_label_datagen.flow_from_directory(label_DIR,
+                                                            target_size=(512, 512),
+                                                            batch_size=BATCH_SIZE,
+                                                            class_mode=None,
+                                                            shuffle=True,
+                                                            seed=1)
+train_generator = zip(train_generator_1, train_generator_2)
 
 class DataGenerator_val(ImageDataGenerator):
     def flow_from_directory(self, directory, *args, **kwargs):
         generator = super().flow_from_directory(directory, class_mode=None, *args, **kwargs)
         while True:
-            ori = next(generator)
+            temporal = next(generator)
 
             data_gen_args = dict(rescale=1. / 255)
             datagen = ImageDataGenerator(**data_gen_args)
@@ -93,33 +102,38 @@ class DataGenerator_val(ImageDataGenerator):
                 batch_size=BATCH_SIZE,
                 target_size=(512, 512),
                 class_mode=None,
+                shuffle=True,
                 seed=1)
 
             masked = next(masked_generator)
 
-            # temporal_generator = datagen.flow_from_directory(
-            #     val_temporal_DIR,
-            #     batch_size=BATCH_SIZE,
-            #     target_size=(512, 512),
-            #     class_mode=None,
-            #     seed=1)
-            # temporal = next(temporal_generator)
-            temporal = deepcopy(ori)
-
             gc.collect()
-            yield [masked, temporal], ori
-
+            yield [masked, temporal]
 
 val_datagen = DataGenerator_val(rescale=1. / 255)
-val_generator = val_datagen.flow_from_directory(
-    val_label_DIR, target_size=(512, 512),batch_size=BATCH_SIZE,seed=1
-)
+val_generator_1 = val_datagen.flow_from_directory(val_temporal_DIR,
+                                                  target_size=(512, 512),
+                                                  batch_size=BATCH_SIZE,
+                                                  shuffle=True,
+                                                  seed=1
+                                                  )
+
+val_gen_args = dict(rescale=1. / 255)
+val_label_datagen = ImageDataGenerator(**val_gen_args)
+val_generator_2 = val_label_datagen.flow_from_directory(val_label_DIR,
+                                                        target_size=(512, 512),
+                                                        batch_size=BATCH_SIZE,
+                                                        class_mode=None,
+                                                        shuffle=True,
+                                                        seed=1)
+val_generator = zip(val_generator_1, val_generator_2)
+
 
 class DataGenerator_test(ImageDataGenerator):
     def flow_from_directory(self, directory, *args, **kwargs):
         generator = super().flow_from_directory(directory, class_mode=None, *args, **kwargs)
         while True:
-            ori = next(generator)
+            temporal = next(generator)
 
             data_gen_args = dict(rescale=1. / 255)
             datagen = ImageDataGenerator(**data_gen_args)
@@ -128,30 +142,37 @@ class DataGenerator_test(ImageDataGenerator):
                 batch_size=BATCH_SIZE,
                 target_size=(512, 512),
                 class_mode=None,
+                shuffle=True,
                 seed=1)
 
             masked = next(masked_generator)
 
-            # temporal_generator = datagen.flow_from_directory(
-            #     test_temporal_DIR,
-            #     batch_size=BATCH_SIZE,
-            #     target_size=(512, 512),
-            #     class_mode=None,
-            #     seed=1)
-            # temporal = next(temporal_generator)
-            temporal = deepcopy(ori)
-
             gc.collect()
-            yield [masked, temporal], ori
+            yield [masked, temporal]
 
 
 test_datagen = DataGenerator_test(rescale=1. / 255)
-test_generator = test_datagen.flow_from_directory(
-    test_label_DIR, target_size=(512, 512),batch_size=BATCH_SIZE,seed=1
-)
+test_generator_1 = test_datagen.flow_from_directory(test_temporal_DIR,
+                                                    target_size=(512, 512),
+                                                    batch_size=BATCH_SIZE,
+                                                    shuffle=True,
+                                                    seed=1
+                                                    )
 
-test_data = next(test_generator)
-(masked, temporal), ori = test_data
+test_gen_args = dict(rescale=1. / 255)
+test_label_datagen = ImageDataGenerator(**test_gen_args)
+test_generator_2 = test_label_datagen.flow_from_directory(test_label_DIR,
+                                                          target_size=(512, 512),
+                                                          batch_size=BATCH_SIZE,
+                                                          class_mode=None,
+                                                          shuffle=True,
+                                                          seed=1)
+test_generator = zip(test_generator_1, test_generator_2)
+
+test_1 = next(test_generator_1)
+test_2 = next(test_generator_2)
+(masked, temporal) = test_1
+ori = test_2
 
 def plot_callback(model):
     """Called at the end of each epoch, displaying our previous test images,
@@ -173,10 +194,10 @@ def plot_callback(model):
         axes[2].set_title('Original Image')
         axes[3].set_title('alluxiary Image')
 
-        plt.savefig(r'D:\STSCNN\test_samples\img_{}_{}.png'.format(i, pred_time))
+        plt.savefig(r'D:\STSCNN\test_samples\test\img_{}_{}.png'.format(i, pred_time))
         plt.close()
 
-model = STSCNN(weight_filepath='D:/STSCNN/logs/')
+model = STSCNN(weight_filepath='D:/STSCNN/logs/test/')
 
 model.fit(
     train_generator,
@@ -186,6 +207,6 @@ model.fit(
     epochs=500,
     plot_callback=plot_callback,
     callbacks=[
-        TensorBoard(log_dir='D:/STSCNN/logs/initial_training', write_graph=False)
+        TensorBoard(log_dir='D:/STSCNN/logs/new_training', write_graph=False)
     ]
 )
